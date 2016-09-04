a729fb8bdfa4a19043d25ce51f72f3b5e22cb6b9
Strays <878605517@qq.com>
2016-09-04 14:43:48 +0800

    Initial Commit

---
+++ DxManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DxManager
+//
+//  Created by Stray on 16/9/4.
+//  Copyright © 2016年 XXTechnology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DxManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DxManager
+//
+//  Created by Stray on 16/9/4.
+//  Copyright © 2016年 XXTechnology Co.,Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
